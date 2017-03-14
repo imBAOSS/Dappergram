6cab6cdc21df2fe435463cbd9051ea8fa0f142fb
imbaoss <baole6989@gmail.com>
2017-03-14 14:55:48 -0700

    Set up session actions

--- frontend/dappergram.jsx
+++ frontend/dappergram.jsx
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
 
+// === For Testing === //
+import * as SessionAPIUtil from './util/session_api_util';
+
+window.signup = SessionAPIUtil.signup;
+window.login = SessionAPIUtil.login;
+window.logout = SessionAPIUtil.logout;
+// =================== //
+
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   const store = configureStore();
 
+  window.store = store;
   ReactDOM.render(<h1>Dappergram</h1>, root);
 });
