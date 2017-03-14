b5c419be523cc62508f7841ddf25ff434fb301bc
imbaoss <baole6989@gmail.com>
2017-03-14 14:03:37 -0700

    Change Autogram to Dappergram in all files

---
+++ frontend/components/root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+
+// react router
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+// react components
+import App from './app';
+
+const Root = store => {
+
+};
