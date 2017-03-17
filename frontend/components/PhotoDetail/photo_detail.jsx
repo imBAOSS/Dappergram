98b092d77cdc787876aee369af23d0a7783624f9
imbaoss <baole6989@gmail.com>
2017-03-17 01:52:18 -0700

    Finish styling on photo detail container

--- frontend/components/PhotoDetail/photo_detail.jsx
+++ frontend/components/PhotoDetail/photo_detail.jsx
@@ -27,25 +27,44 @@ class PhotoDetail extends React.Component {
         <div className="photo">
           <img src={`${this.props.photo.photo_url}`}/>
         </div>
-
-        <div className="num-likes">
-          9001 likes!
-        </div>
-
-        <div className='photo-caption'>
-          Photo caption goes here
-        </div>
-
-        <div className='comments'>
-          Comments go here
-        </div>
-
-        <div className='interact'>
-          <div className='like'>
-            Like
+        <div className='photo-info'>
+          <div className="num-likes">
+            9001 likes!
           </div>
-          <div className='add-comment'>
-            Add Comment
+
+          <div className='photo-caption'>
+            <p>
+              <Link
+                className='user-photo-caption'>
+                {this.props.photo.username}
+              </Link>
+              Photo caption goes here
+            </p>
+          </div>
+
+          <div className='comments'>
+            <p>
+              <Link
+                className='commenter-user'>
+                Commenter
+              </Link>
+              Looking daaaaaaaapper
+            </p>
+          </div>
+
+          <div className='interact'>
+            <div className='like-icon-cont'>
+              <div className='like-icon'>
+              </div>
+            </div>
+            <div className='add-comment-cont'>
+              <form>
+                <input
+                  type='text'
+                  className='add-comment'
+                  placeholder='Add a comment...'/>
+              </form>
+            </div>
           </div>
         </div>
       </div>
