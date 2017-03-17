704df91c64d276bb3694d136908c2e18e37cc740
imbaoss <baole6989@gmail.com>
2017-03-16 20:32:50 -0700

    Seed database with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-photo_list = File.readlines("../../photo_file_names.txt").map(&:chomp)
+photo_list = File.readlines(File.join(Rails.root, 'db', 'photo_file_names.txt')).first.split('\r')
 prefix = 'https://s3.amazonaws.com/dappergram-dev/users/photos/'
 photos = photo_list.map { |file_name| prefix + file_name}
 
-profile_list = File.readlines("../../profile_photo_names.txt").map(&:chomp)
+profile_list = File.readlines(File.join(Rails.root, 'db', 'profile_photo_names.txt')).first.split('\r')
 profile_photos = profile_list.map { |file_name| prefix + file_name}
 
 User.create!(
