81ca5af76adeda348db2b4014b64cc01d868dcfd
imbaoss <baole6989@gmail.com>
2017-03-18 17:44:27 -0700

    Add unique combination migration to likes

--- db/migrate/20170319004030_create_likes.rb
+++ db/migrate/20170319004030_create_likes.rb
@@ -1,10 +1,12 @@
 class CreateLikes < ActiveRecord::Migration[5.0]
   def change
     create_table :likes do |t|
-      t.integer :user_id
-      t.integer :photo_id
+      t.integer :user_id, null: false
+      t.integer :photo_id, null: false
 
       t.timestamps
     end
+
+    add_index :likes, [:user_id, :photo_id], unique: true
   end
 end
