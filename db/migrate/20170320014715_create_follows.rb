b29bdc76f476de38be49507a7536a5e5bfb8af5e
imbaoss <baole6989@gmail.com>
2017-03-20 07:47:48 -0700

    Change column name for heroku migration

--- db/migrate/20170320014715_create_follows.rb
+++ db/migrate/20170320014715_create_follows.rb
@@ -2,11 +2,11 @@ class CreateFollows < ActiveRecord::Migration[5.0]
   def change
     create_table :follows do |t|
       t.integer :follower_id, null: false
-      t.integer :followee_id, null: false
+      t.integer :following_id, null: false
 
       t.timestamps
     end
 
-    add_index :follows, [:follower_id, :followee_id], unique: true
+    add_index :follows, [:follower_id, :following_id], unique: true
   end
 end
