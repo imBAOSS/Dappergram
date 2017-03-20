00cd2b09e53b6ecb500fd8328e80607721158bc9
imbaoss <baole6989@gmail.com>
2017-03-19 23:00:10 -0700

    Finish comments controller and show view

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,23 @@
 class Api::CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.user_id = current_user.id
+    @comment.photo_id = params[:photo_id]
+
+    if @comment.save
+      render 'api/comments/show', status: 200
+    else
+      render json: @comment.errors.full_messages, status: 422
+  end
+
+  def destroy
+    @comment = Comment.find_by(user_id: current_user.id, photo_id: params[:photo_id])
+    @comment.destroy! if @comment
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, :photo_id, :body)
+  end
 end
