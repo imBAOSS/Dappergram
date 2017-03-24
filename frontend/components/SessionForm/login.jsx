0e2099b9a2ab4fc0b0bbbe9a38d1ad2316b88801
imbaoss <baole6989@gmail.com>
2017-03-24 00:36:47 -0700

    Wrote auto guest login

--- frontend/components/SessionForm/login.jsx
+++ frontend/components/SessionForm/login.jsx
@@ -5,11 +5,14 @@ class LogInForm extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {username: '', password: ''};
+    this.state = {username: '', password: '', loginGuest: false};
     this.update = this.update.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.guestLogIn = this.guestLogIn.bind(this);
     this.listErrors = this.listErrors.bind(this);
+    this.writeUsername = this.writeUsername.bind(this);
+    this.writePassword = this.writePassword.bind(this);
+    this.demoLogin = this.demoLogin.bind(this);
   }
 
   update(field) {
@@ -37,6 +40,47 @@ class LogInForm extends React.Component {
     this.props.login(user);
   }
 
+  componentWillReceiveProps() {
+    if (!this.state.loginGuest) {
+      this.setState({
+        loginGuest: true
+      })
+    }
+  }
+
+  writeUsername(usernameIdx = 0, username = 'guest') {
+    if (usernameIdx > username.length) {
+      return this.writePassword();
+    } else {
+      setTimeout(() => {
+        this.setState({
+          username: username.slice(0, usernameIdx)
+        });
+        this.writeUsername(usernameIdx + 1, 'guest');
+      }, 70);
+    }
+  }
+
+  writePassword(passwordIdx = 0, password = 'guest_password') {
+    if (passwordIdx > password.length) {
+      this.setState({
+        loginGuest: false
+      })
+      return this.props.login(this.state).then(() => this.props.router.push("/feed"));
+    } else {
+      setTimeout(() => {
+        this.setState({
+          password: password.slice(0, passwordIdx)
+        });
+        this.writePassword(passwordIdx + 1, 'guest_password');
+      }, 70);
+    }
+  }
+
+  demoLogin() {
+    this.writeUsername();
+  }
+
   listErrors() {
     if (this.props.errors.length > 0) {
       return (
@@ -58,7 +102,7 @@ class LogInForm extends React.Component {
         </div>
         <div>
           <button
-            onClick={this.guestLogIn}
+            onClick={this.demoLogin}
             className="button">Login as Guest</button>
         </div>
         <div className="or-line">
